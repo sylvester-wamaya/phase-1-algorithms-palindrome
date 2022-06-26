a86c5283de7b9bbf16a97b1fde846ea7e6039803
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-26 20:22:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  word.toLowerCase();
+  let drow = word.split("").reverse().join("");
+  return word === drow;
 }
 
 /* 
   Add your pseudocode here
+
+  change the word to lowercases
+  reverse the words and check if it's a palindrome
 */
 
 /*
   Add written explanation of your solution here
+  the code reverses the string word, rejoins and checks if same as the first word.
 */
 
+console.log("=>"), isPalindrome("dad")
+console.log("=>"), isPalindrome("bro")
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
